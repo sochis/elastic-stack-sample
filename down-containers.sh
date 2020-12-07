9d2fbe62c116b362dc880fae8cf4d1d6a2bfeec4
sochis <seiichiro.ochi@gmail.com>
2020-12-07 16:09:12 +0900

    Update down containers sh

--- down-containers.sh
+++ down-containers.sh
@@ -8,4 +8,5 @@ docker-compose -f beats/filebeat/docker-compose.yml down
 docker-compose -f beats/auditbeat/docker-compose.yml down
 docker-compose -f logstash/docker-compose.yml down
 docker-compose -f kibana/docker-compose.yml down
+docker-compose -f enterprise-search/docker-compose.yml down
 docker-compose -f elasticsearch/docker-compose.yml down
